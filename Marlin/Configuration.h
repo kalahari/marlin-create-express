ea9504701ce8bc5d808ce0f0ebb7f4b3599c428d
Blake Mitchell <blake@barkingspoon.com>
2018-02-26 14:26:45 -0800

    Slower X/Y when auto-leveling

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -531,7 +531,7 @@
  * Override with M203
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 500, 500, 5, 25 }
+#define DEFAULT_MAX_FEEDRATE          { 185, 185, 5, 25 }
 
 /**
  * Default Max Acceleration (change/s) change = mm/s
